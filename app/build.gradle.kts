572974a27531968e63032cfc547240b123592817
isabelamarchesoni <isamarchesoniserra@gmail.com>
2026-06-03 00:06:14 -0300

    feat: update visual theme and add book icon to screens
    - Change color palette to library theme (brown and cream tones)
    - Add ImageView with book icon (book.png) on top of each screen
    - Center title, form fields and button in both activities
    - Add rounded corners to cadastro button
    - Remove horizontal_bias auto-generated by visual editor
    - Add MaterialComponents dependency in build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.telaskotlin"
+    namespace = "br.edu.fatecpg.livros"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
